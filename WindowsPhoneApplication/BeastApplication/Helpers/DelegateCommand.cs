1d163e1a656043f0ece2ac294d7e8896fc5d9130
mpenchev86 <peace.glory@yahoo.com>
2015-12-18 00:44:55 +0200

    added folder Helpers and class DelegateCommand.cs

---
+++ WindowsPhoneApplication/BeastApplication/Helpers/DelegateCommand.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace BeastApplication.Helpers
+{
+    public class DelegateCommand : ICommand
+    {
+        private Func<bool> canExecute;
+        private Action execute;
+
+        public DelegateCommand(Action execute, Func<bool> canExecute = null)
+        {
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            if (this.canExecute == null)
+            {
+                return true;
+            }
+            return this.canExecute();
+        }
+
+        public void Execute(object parameter)
+        {
+            this.execute();
+        }
+    }
+}
