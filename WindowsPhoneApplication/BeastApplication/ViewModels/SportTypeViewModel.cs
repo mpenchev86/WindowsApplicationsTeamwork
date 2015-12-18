97eaff55272e5edb4074cdb23a36e3ca697c0cd7
ReniGetskova <reni.getskova@gmail.com>
2015-12-18 02:18:06 +0200

    Added SportTypeViewModel.cs after build solution!

--- WindowsPhoneApplication/BeastApplication/ViewModels/SportTypeViewModel.cs
+++ WindowsPhoneApplication/BeastApplication/ViewModels/SportTypeViewModel.cs
@@ -6,7 +6,10 @@ using System.Threading.Tasks;
 
 namespace BeastApplication.ViewModels
 {
-    class SportType
+    public class SportTypeViewModel
     {
+        public string Name { get; set; }
+
+        public string ImagePath { get; set; }
     }
 }
